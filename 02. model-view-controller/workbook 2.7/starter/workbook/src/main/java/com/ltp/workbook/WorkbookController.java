85d6ddfc8da3197757afdc95694992c46b773d3c
Kelvin <kelvinsantosdealmeida@gmail.com>
2024-05-17 17:43:18 -0300

    secton 02

--- 02. model-view-controller/workbook 2.7/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
+++ 02. model-view-controller/workbook 2.7/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -6,14 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WorkbookController {
-
-    @GetMapping(value="/")
-    public String getMethodName(Model model) {
-
-        model.addAttribute("budget", 10000);
-        model.addAttribute("make", "Subaru");
-
-        return "dealership";
-    }
     
+    @GetMapping("/")
+    public String getName(Model model) {
+        model.addAttribute("name", "Kelvin");
+        return "name-list";
+    }
 }
