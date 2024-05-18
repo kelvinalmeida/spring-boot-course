b6528484951fcdb04fc6cf89805d49ba342db325
Kelvin <kelvinsantosdealmeida@gmail.com>
2024-05-18 13:58:26 -0300

    section 02

--- 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,5 +1,8 @@
 package com.ltp.gradesubmission;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +11,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GradeController {
     
+    List<Grade> studentGrade = new ArrayList<>(); 
+
     @GetMapping("/grade")
     public String gerGrades(Model model) {
-        Grade grade = new Grade("Harry", "Portions", "C-"); // POJO
-        model.addAttribute("grade", grade);
+
+        studentGrade.add(new Grade("Harry", "Portions", "C-"));
+        studentGrade.add(new Grade("Kelvin", "Ingles", "A+"));
+        studentGrade.add(new Grade("Ana", "Ciências", "A"));
+
+        model.addAttribute("grades", studentGrade);
 
         return "grades";
     }
